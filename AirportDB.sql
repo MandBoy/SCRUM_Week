fdf63f015ed471b486ae3ba78889c425b2ebfe74
Andreas Lind <32731570+MandBoy@users.noreply.github.com>
2019-10-21 14:37:12 +0200

    Start Database idea.

--- AirportDB.sql
+++ AirportDB.sql
@@ -1,12 +1,24 @@
-Create Table Administration(
+Create Table Flight(
 Id int NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-destination varchar(255),
-arrival varchar(255),
-departure varchar(255)
+Departure Timestamp NOT NULL,
+Arrival Timestamp NOT NULL,
+FOREIGN KEY Plane(Id) REFERENCES Planes(PlaneId),
+FOREIGN KEY Airport(Id) REFERENCES Airports(Stall)  
 );
 
 Create Table Plane(
+Id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+StartLocation varchar(255),
+Destination varchar(255),
+Size int(3)    
+);
+
+Create Table Airport(
+Id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+Name varchar(255),
+Stall int
+);
 
 
-)
+
 
