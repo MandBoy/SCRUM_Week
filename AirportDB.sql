aa7e4a9fd7a907160eac512335e06003d150ae5a
Andreas Lind <32731570+MandBoy@users.noreply.github.com>
2019-10-21 13:33:27 +0200

    Start of DB.

---
+++ AirportDB.sql
@@ -0,0 +1,12 @@
+Create Table Administration(
+Id int NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+destination varchar(255),
+arrival varchar(255),
+departure varchar(255)
+);
+
+Create Table Plane(
+
+
+)
+
