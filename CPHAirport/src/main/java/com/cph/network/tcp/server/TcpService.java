c510f5b66055c06b400aadda4a2503d87858e9b2
Andreas Petersen <andreas.petersen01@gmail.com>
2019-10-23 14:45:29 +0200

    Update TcpService.java

--- CPHAirport/src/main/java/com/cph/network/tcp/server/TcpService.java
+++ CPHAirport/src/main/java/com/cph/network/tcp/server/TcpService.java
@@ -1,18 +1,62 @@
-package com.cph.network.tcp.server;
+package com.cph.network.tcp.client;
 
 import com.cph.network.tcp.shared.IMessageHandler;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
 public class TcpService
 {
-    public TcpService() {
+    private String host;
+    private int port;
 
+    private volatile boolean listening;
+
+    private IMessageHandler handler;
+    private Socket listener;
+    private BufferedReader bufferedReader;
+
+    public TcpService(String host, int port) {
+        this.host = host;
+        this.port = port;
+
+        try {
+            listen(handler);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void listen(IMessageHandler handler) {
+    public void listen(IMessageHandler handler) throws IOException {
+        if (listening) {
+            return;
+        }
+
+        listener = new Socket(host, port);
+        listener.setSoTimeout(5000);
+        listening = true;
+
+        bufferedReader = new BufferedReader(new InputStreamReader(listener.getInputStream()));
+
         String message = null;
 
-        // get message through socket
+        try {
+            while (listening) {
+                if ((message = bufferedReader.readLine()) != null) {
+                    handler.handleMessage(message);
+                }
+            }
+        } catch (SocketTimeoutException e) {
+            //empty
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-        handler.handleMessage(message);
+    public void stop() {
+        listening = false;
     }
 }
