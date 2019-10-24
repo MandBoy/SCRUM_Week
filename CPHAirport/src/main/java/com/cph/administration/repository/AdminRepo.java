d3672bfca9d6aa66e3ef4d9c3c0affa9c24052ad
Sofie Vonge Jensen <sofievj_89@hotmail.com>
2019-10-24 13:27:39 +0200

    Flight info from db

--- CPHAirport/src/main/java/com/cph/administration/repository/AdminRepo.java
+++ CPHAirport/src/main/java/com/cph/administration/repository/AdminRepo.java
@@ -1,13 +1,11 @@
 package com.cph.administration.repository;
 
+import com.cph.models.Flight;
 import com.cph.models.Station;
 import com.cph.models.StationInfo;
 import com.cph.persistence.sql.ConnectionFactory;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,9 +19,46 @@ public class AdminRepo {
 
     private final String SELECT_ALL_STALLS = "SELECT * FROM station";
     private final String SELECT_ALL_STALLS_WITH_FLIGHTS = "SELECT flight.serial_number, station.id "
-    + "FROM flight_station JOIN station ON flight_station.fk_station_id = station.id "
-    + "JOIN flight ON flight.id = flight_station.fk_flight_id "
-    + "WHERE flight_station.reserved_to = null";
+    + "FROM flight__station JOIN station ON flight__station.fk_station_id = station.id "
+    + "JOIN flight ON flight.id = flight__station.fk_flight_id "
+    + "WHERE flight__station.reserved_to IS NULL";
+    private final String SELECT_ALL_FLIGHTS = "SELECT * FROM flight";
+
+
+    public List<Flight> seeAllFlightInfo()
+    {
+        List<Flight> flights = new ArrayList<>();
+
+        try {
+            Connection conn = ConnectionFactory.createNewConnection();
+            Statement st = conn.createStatement();
+            ResultSet res = st.executeQuery(SELECT_ALL_FLIGHTS);
+
+
+            while(res.next())
+            {
+                int id = res.getInt("id");
+                String serialNo = res.getString("route_number");
+                Timestamp arrival = res.getTimestamp("arrival_time");
+                Timestamp depature = res.getTimestamp("departure_time");
+                int arrivalOffset = res.getInt("arrival_offset");
+                int departureOffset = res.getInt("departure_offset");
+                String fromDestination = res.getString("from_destination");
+                String toDestination = res.getString("to_destination");
+
+
+                flights.add(new Flight(id, serialNo, arrival, depature, arrivalOffset, departureOffset, fromDestination, toDestination));
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        return flights;
+    }
+
 
 
 
