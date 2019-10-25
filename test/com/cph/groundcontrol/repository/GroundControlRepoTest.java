b7b6cc1bda817e64bac0a3e27e87c8fd0258c106
dani153i <danielblom90@gmail.com>
2019-10-25 15:23:08 +0200

    Update GroundControlRepoTest.java

--- test/com/cph/groundcontrol/repository/GroundControlRepoTest.java
+++ test/com/cph/groundcontrol/repository/GroundControlRepoTest.java
@@ -8,25 +8,50 @@ import org.junit.jupiter.api.Test;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-public class GroundControlRepoTest extends GroundRepo
+public class GroundControlRepoTest
 {
+    protected final String CREATE_TEMP_FS =
+            "CREATE TEMPORARY TABLE ABC AS " +
+                    "SELECT * FROM flight__station " +
+                    "WHERE " +
+                    "fk_station_id = ?;";
+
+    protected final String UPDATE_FLIGHT_STATION =
+                    "UPDATE flight__station " +
+                    "SET fk_station_id = ? " +
+                    "WHERE fk_flight_id = ? AND (SELECT COUNT(*) FROM ABC) = 0;";
+
+    protected final String DROP_TEMP_FS =
+            "DROP TABLE ABC;";
 
     @Test
     public void updateFlightStation() {
         int fk_flightId = 1;
-        int fk_stationId = 3;
+        int fk_stationId = 1;
+
 
         try {
             Connection connection = ConnectionFactory.createNewConnection();
-            PreparedStatement preStatemnt = connection.prepareStatement(UPDATE_FLIGHT_STATION);
+            PreparedStatement preStatemnt1 = connection.prepareStatement(CREATE_TEMP_FS);
+            PreparedStatement preStatemnt2 = connection.prepareStatement(UPDATE_FLIGHT_STATION);
 
-            preStatemnt.setInt(0, fk_flightId);
-            preStatemnt.setInt(1, fk_stationId);
+            Statement statement = connection.createStatement();
 
-            preStatemnt.executeQuery();
+            preStatemnt1.setInt(1, fk_stationId);
 
-// hej
+            preStatemnt2.setInt(1, fk_stationId);
+            preStatemnt2.setInt(2, fk_flightId);
+
+
+            preStatemnt1.execute();
+            preStatemnt2.execute();
+            //statement.executeQuery(DROP_TEMP_FS);
+
+
+
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
